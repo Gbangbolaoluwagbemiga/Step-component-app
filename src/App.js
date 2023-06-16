39924c766e9bbf8a58dd2edf6fddbc6889cb0830
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-06-16 10:44:04 +0100

    modifications of the app

--- src/App.js
+++ src/App.js
@@ -54,14 +54,23 @@ function Steps() {
           </StepMessage>
 
           <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-              <span>👈</span> Previous
-            </Button>
+            {step >= 2 && (
+              <Button
+                bgColor="#7950f2"
+                textColor="#fff"
+                onClick={handlePrevious}
+              >
+                <span>👈</span> Previous
+              </Button>
+            )}
 
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              Next <span>👉</span>
-              <span>🤓</span>
-            </Button>
+            {step <= 2 && <div></div>}
+            {step <= 2 && (
+              <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+                Next <span>👉</span>
+                <span>🤓</span>
+              </Button>
+            )}
           </div>
         </div>
       )}
